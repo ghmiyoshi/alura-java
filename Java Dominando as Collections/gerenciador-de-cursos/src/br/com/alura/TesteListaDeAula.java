c79e92f8d18e761286be98b611cb9d2bf8a5a0ae
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2019-04-03 00:21:20 -0300

    Java dominando as Collections

---
+++ Java Dominando as Collections/gerenciador-de-cursos/src/br/com/alura/TesteListaDeAula.java
@@ -0,0 +1,31 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TesteListaDeAula {
+
+	public static void main(String[] args) {
+		Aula aula1 = new Aula("Revisando as ArrayLists", 21);
+		Aula aula2 = new Aula("Listas de objetos", 20);
+		Aula aula3 = new Aula("Relacionamento de listas e objetos", 15);
+
+		ArrayList<Aula> aulas = new ArrayList<>();
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+
+		System.out.println(aulas);
+
+		Collections.sort(aulas);
+		System.out.println(aulas);
+
+		Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
+
+		System.out.println(aulas);
+
+		aulas.sort(Comparator.comparing(Aula::getTempo));
+
+	}
+}
